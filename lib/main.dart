13c96253cdd4c09bcef9af10c61f8be0635d4c3f
Tursunali008 <153731500+Tursunali008@users.noreply.github.com>
2024-07-18 21:07:08 +0500

    dasda

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,27 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vazifa25/bloc/bloc_obthorver.dart';
 import 'package:vazifa25/bloc/currency_bloc.dart';
+import 'package:vazifa25/bloc/currency_event.dart';
 import 'package:vazifa25/logic/repostories/currency_repostory.dart';
 import 'package:vazifa25/ui/screens/home_screen.dart';
 
 void main() {
+  Bloc.observer = CurrencyBlocObserver();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final CurrencyRepository currencyRepository = CurrencyRepository();
-  MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final currencyRepository = CurrencyRepository();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Currency Converter',
       home: BlocProvider(
-        create: (context) => CurrencyBloc(currencyRepository),
+        create: (context) =>
+            CurrencyBloc(currencyRepository)..add(FetchCurrencies()),
         child: HomeScreen(),
       ),
     );
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,12 +1,13 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vazifa25/bloc/currency_bloc.dart';
 import 'package:vazifa25/bloc/currency_event.dart';
 import 'package:vazifa25/bloc/currency_state.dart';
 import 'package:vazifa25/logic/model/currensy.dart';
+
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +38,7 @@ class HomeScreen extends StatelessWidget {
 class CurrencyConverter extends StatefulWidget {
   final List<Currency> currencies;
 
-  CurrencyConverter({required this.currencies});
+  const CurrencyConverter({super.key, required this.currencies});
 
   @override
   _CurrencyConverterState createState() => _CurrencyConverterState();
@@ -47,6 +48,24 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
   String? _selectedCurrency;
   double _inputAmount = 1.0;
   double? _convertedAmount;
+  TextEditingController _searchController = TextEditingController();
+  List<Currency> _filteredCurrencies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredCurrencies = widget.currencies;
+    _searchController.addListener(_filterCurrencies);
+  }
+
+  void _filterCurrencies() {
+    final query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredCurrencies = widget.currencies
+          .where((currency) => currency.code.toLowerCase().contains(query))
+          .toList();
+    });
+  }
 
   void _convertCurrency() {
     if (_selectedCurrency == null) {
@@ -63,60 +82,78 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
   }
 
   @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          DropdownButtonFormField<String>(
-            value: _selectedCurrency,
-            hint: const Text("Select Currency"),
-            onChanged: (value) {
-              setState(() {
-                _selectedCurrency = value;
-              });
-            },
-            items: widget.currencies
-                .map((currency) => DropdownMenuItem(
-                      value: currency.code,
-                      child: Text(currency.code),
-                    ))
-                .toList(),
-          ),
-          TextField(
-            decoration: const InputDecoration(
-                hintText: "Amount", border: OutlineInputBorder()),
-            keyboardType: TextInputType.number,
-            onChanged: (value) {
-              setState(() {
-                _inputAmount = double.tryParse(value) ?? 1.0;
-              });
-            },
-          ),
-          const SizedBox(height: 200),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-            onPressed: _convertCurrency,
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.change_circle,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Convert",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                  hintText: "Amount", border: OutlineInputBorder()),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  _inputAmount = double.tryParse(value) ?? 1.0;
+                });
+              },
             ),
-          ),
-          if (_convertedAmount != null)
-            Text(
-              "Converted Amount: $_convertedAmount UZS",
-              style: const TextStyle(fontSize: 20),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                  hintText: "Search Currency", border: OutlineInputBorder()),
             ),
-        ],
+            const SizedBox(height: 16),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+              onPressed: _convertCurrency,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Convert",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            if (_convertedAmount != null)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Text(
+                  "Converted Amount: $_convertedAmount UZS",
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+            const SizedBox(height: 16),
+            Container(
+              height: 500,
+              child: ListView.builder(
+                itemCount: _filteredCurrencies.length,
+                itemBuilder: (context, index) {
+                  final currency = _filteredCurrencies[index];
+                  return ListTile(
+                    title: Text(currency.code),
+                    subtitle: Text('Rate: ${currency.rate}'),
+                    onTap: () {
+                      setState(() {
+                        _selectedCurrency = currency.code;
+                        _convertCurrency();
+                      });
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
